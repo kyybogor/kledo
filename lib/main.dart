b407db5145f308529c603791776b86b2a178a6d2
kyybogor <kyybogor001@gmail.com>
2025-04-08 10:30:28 +0700

    iki bogor

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Dashboardscreen(), // ← Ganti halaman pertama di sini
+      home: const Dashboardscreen(),
     );
   }
 }
