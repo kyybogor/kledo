e7874353762ca22e72d859a4ad4ed898a02c6a10
azkafr2008 <azkafr2008@gmail.com>
2025-04-28 11:52:48 +0700

    p

--- lib/laporan/neracalaporanscreen.dart
+++ lib/laporan/neracalaporanscreen.dart
@@ -1,13 +1,48 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_kledo/laporan/laporanscreen.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart'; // Import intl untuk format tanggal
 
-class NeracaPage extends StatelessWidget {
+// ApiService untuk mengambil data dari API
+class ApiService {
+  final String apiUrl = "http://192.168.1.4/hiyami/neraca.php"; // URL API
+
+  Future<List<Map<String, dynamic>>> fetchNeracaData() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        List<dynamic> data = json.decode(response.body);
+        return data.map((item) => item as Map<String, dynamic>).toList();
+      } else {
+        throw Exception('Gagal mengambil data');
+      }
+    } catch (e) {
+      throw Exception('Terjadi kesalahan: $e');
+    }
+  }
+}
+
+// Halaman Neraca untuk menampilkan data
+class NeracaPage extends StatefulWidget {
+  @override
+  _NeracaPageState createState() => _NeracaPageState();
+}
+
+class _NeracaPageState extends State<NeracaPage> {
   final TextStyle labelStyle = const TextStyle(fontSize: 16);
-  final TextStyle valueStyle =
-      const TextStyle(fontSize: 16, color: Colors.blue);
-  final TextStyle boldStyle =
-      const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+  final TextStyle valueStyle = const TextStyle(fontSize: 16, color: Colors.blue);
+  final TextStyle boldStyle = const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
 
+  late Future<List<Map<String, dynamic>>> _neracaData;
+
+  @override
+  void initState() {
+    super.initState();
+    _neracaData = ApiService().fetchNeracaData(); // Ambil data dari API
+  }
+
+  // Widget untuk menampilkan header setiap section
   Widget buildSectionHeader(String title) {
     return Container(
       width: double.infinity,
@@ -20,7 +55,11 @@ class NeracaPage extends StatelessWidget {
     );
   }
 
+  // Widget untuk menampilkan item label dan value
   Widget buildItem(String label, String value, {bool isBold = false}) {
+    // Menentukan warna berdasarkan label
+    Color valueColor = (label == "Total Assets") ? Colors.black : Colors.blue;
+
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
       decoration: BoxDecoration(
@@ -43,7 +82,7 @@ class NeracaPage extends StatelessWidget {
             flex: 1,
             child: Text(
               value,
-              style: isBold ? boldStyle : valueStyle,
+              style: isBold ? boldStyle : valueStyle.copyWith(color: valueColor), // Mengubah warna teks
               textAlign: TextAlign.right,
               overflow: TextOverflow.ellipsis,
               softWrap: false,
@@ -52,6 +91,13 @@ class NeracaPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  // Fungsi untuk mendapatkan tanggal hari ini
+  String getFormattedDate() {
+    final DateTime now = DateTime.now();
+    final DateFormat formatter = DateFormat('dd MMMM yyyy');
+    return formatter.format(now);
   }
 
   @override
@@ -66,128 +112,85 @@ class NeracaPage extends StatelessWidget {
         ),
         title: const Text("Neraca"),
         centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.filter_list),
-            onPressed: () {},
-          )
-        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(bottom: 80),
-        child: ListView(
-          children: [
-            Container(
-              width: double.infinity,
-              color: Colors.yellow[100],
-              padding: const EdgeInsets.all(12),
-              child: RichText(
-                text: TextSpan(
-                  style: const TextStyle(fontSize: 14, color: Colors.black),
+      body: FutureBuilder<List<Map<String, dynamic>>>(  // FutureBuilder to fetch data from API
+        future: _neracaData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text("Tidak ada data"));
+          }
+
+          var data = snapshot.data!;
+
+          // Mengelompokkan data berdasarkan section
+          Map<String, List<Map<String, dynamic>>> groupedData = {};
+          for (var item in data) {
+            if (!groupedData.containsKey(item['section'])) {
+              groupedData[item['section']] = [];
+            }
+            groupedData[item['section']]!.add(item);
+          }
+
+          return ListView(
+            children: groupedData.keys.map((section) {
+              List<Map<String, dynamic>> sectionData = groupedData[section]!;
+
+              // Jika section adalah "Total", hanya tampilkan Total Assets tanpa header
+              if (section == "Total") {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: sectionData.map((item) {
+                    // Menampilkan hanya Total Assets
+                    return buildItem(item['label'], item['value']);
+                  }).toList(),
+                );
+              }
+
+              // Menambahkan tanggal di atas header Liabilitas Jangka Pendek dan Kas & Bank
+              if (section == "Liabilitas Jangka Pendek" || section == "Kas & Bank") {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const TextSpan(
-                        text:
-                            "Data yang tampil saat ini adalah data dummy. Setelah Anda siap, "),
-                    TextSpan(
-                      text: "klik disini",
-                      style: const TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline),
+                    // Tanggal kecil di atas header
+                    Container(
+                      padding: const EdgeInsets.only(left: 12, bottom: 0, top: 12), // Mengatur jarak
+                      child: Text(
+                        getFormattedDate(),  // Menampilkan tanggal
+                        style: TextStyle(fontSize: 12, color: Colors.black),  // Ukuran font lebih kecil
+                      ),
                     ),
-                    const TextSpan(text: " untuk mengosongkan data."),
+                    buildSectionHeader(section),  // Menampilkan header section
+                    ...sectionData.map((item) {
+                      return buildItem(item['label'], item['value']);
+                    }).toList(),
                   ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text("Assets 25/04/2025",
-                  style: TextStyle(fontSize: 14, color: Colors.grey[600])),
-            ),
-            const SizedBox(height: 10),
-            buildSectionHeader("Kas & Bank"),
-            buildItem("Kas", "28.900.519"),
-            buildItem("Rekening Bank", "29.421.935"),
-            buildItem("Giro", "30.456.920"),
-            buildItem("Total Kas & Bank", "88.779.374", isBold: true),
-            buildSectionHeader("Aset Lancar"),
-            buildItem("Piutang Usaha", "50.342.590"),
-            buildItem("Cadangan Kerugian Piutang", "10.125.780"),
-            buildItem("Persediaan Barang", "18.123.000"),
-            buildItem("Aset Lancar Lainnya", "10.811"),
-            buildItem("PPN Masukan", "3.999.000"),
-            buildItem("Total Aset Lancar", "83.151.682", isBold: true),
-            buildSectionHeader("Aset Tetap"),
-            buildItem("Aset Tetap-Tanah", "35.100.000"),
-            buildItem("Aset Tak Berwujud", "(47.748)"),
-            buildItem("Total Aset Tetap", "35.052.252", isBold: true),
-            buildSectionHeader("Depresiasi & Amortisasi"),
-            buildItem("Akumulasi Penyusutan-Mesin & Peralatan", "(52.252)"),
-            buildItem("Total Depresiasi & Amortisasi", "(52.252)",
-                isBold: true),
-            buildSectionHeader("Lainnya"),
-            buildItem("Investasi", "(89.189)"),
-            buildItem("Total Lainnya", "(89.189)", isBold: true),
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text("Total Assets 206.841.867", style: boldStyle),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text("Liabilitas & Modal 25/04/2025",
-                  style: TextStyle(fontSize: 14, color: Colors.grey[600])),
-            ),
-            const SizedBox(height: 10),
-            buildSectionHeader("Liabilitas Jangka Pendek"),
-            buildItem("Hutang Usaha", "42.879.684"),
-            buildItem("Hutang Belum Ditagih", "8.499.009"),
-            buildItem("Hutang Deviden", "(44.144)"),
-            buildItem("Pendapatan Diterima Di Muka", "1.802"),
-            buildItem("PPN Keluaran", "13.095.085"),
-            buildItem("Hutang Pajak-PPh 22", "(34.234)"),
-            buildItem("Hutang Pajak-PPh 29", "(63.964)"),
-            buildItem("Kewajiban Lancar Lainnya", "45.946"),
-            buildItem("Total Liabilitas Jangka Pendek", "64.379.184",
-                isBold: true),
-            buildSectionHeader("Liabilitas Jangka Panjang"),
-            buildItem("Total Liabilitas Jangka Panjang", "0"),
-            buildSectionHeader("Perubahan Modal"),
-            buildItem("Modal Saham", "92.757.658"),
-            buildItem("Tambahan Modal Disetor", "5.405"),
-            buildItem("Laba ditahan", "81.982"),
-            buildItem("Pendapatan Sampai Periode Terakhir", "0"),
-            buildItem("Pendapatan Periode Ini", "49.617.639"),
-            buildItem("total Perubahan Modal", "142.462.684"),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text("Total Liabilitas & Modal 206.841.867",
-                  style: boldStyle),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            heroTag: "share",
-            child: const Icon(Icons.share),
-            onPressed: () {},
-            backgroundColor: Colors.blueAccent,
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            heroTag: "download",
-            child: const Icon(Icons.download),
-            onPressed: () {},
-            backgroundColor: Colors.blue,
-          ),
-        ],
+                );
+              }
+
+              // Menampilkan header untuk section lain selain "Total"
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  buildSectionHeader(section),  // Menampilkan header section
+                  ...sectionData.map((item) {
+                    return buildItem(item['label'], item['value']);
+                  }).toList(),
+                ],
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: NeracaPage(),
+  ));
 }
