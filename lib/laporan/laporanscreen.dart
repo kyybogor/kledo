4e9b08a5aeddd064585d04ea170af1c1e5f264cf
kyybogor <kyybogor001@gmail.com>
2025-05-14 11:03:20 +0700

    iki

--- lib/laporan/laporanscreen.dart
+++ lib/laporan/laporanscreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_kledo/Dashboard/dashboardscreen.dart';
 import 'package:flutter_application_kledo/inventori/ringkasaninventori.dart';
 import 'package:flutter_application_kledo/inventori/ringkasanstokgudang.dart';
 import 'package:flutter_application_kledo/laporan/aruskasscreen.dart';
+import 'package:flutter_application_kledo/laporan/biayaperkontak.dart';
 import 'package:flutter_application_kledo/laporan/bukubesar.dart';
 import 'package:flutter_application_kledo/laporan/jurnal.dart';
 import 'package:flutter_application_kledo/laporan/penjualan/detailpenjualan.dart';
@@ -337,6 +338,13 @@ class LaporanPage extends StatelessWidget {
                                       context,
                                       MaterialPageRoute(
                                           builder: (context) => const UmurPiutangPage(),
+                                    )
+                                  );
+                                } else if(laporan == "Biaya per Kontak"){
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => const Biayaperkontak(),
                                     )
                                   );
                                 }
